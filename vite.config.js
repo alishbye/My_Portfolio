88632d6354732a506db58ca5fe49f26c2c54c1b9
alishbye <alishba903.sie@gmail.com>
2025-09-20 20:03:42 -0700

    FIX

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/My_Portfolio/' : '/',
+  base: '/My_Portfolio/', // ✅ GitHub Pages path
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-  }
+  },
 })
