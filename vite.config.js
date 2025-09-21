a6aa44dc8afa4d655cdfdf3076687d51d4ae1f0f
alishbye <alishba903.sie@gmail.com>
2025-09-20 20:18:04 -0700

    fixings

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/My_Portfolio/', // ✅ GitHub Pages path
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+  plugins: [react(), tailwindcss()], 
+  base: "/My_Portfolio/",
 })
